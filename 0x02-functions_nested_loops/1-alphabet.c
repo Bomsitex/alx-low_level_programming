442cf51482d06299873de85eb2b88a95a0ac8f6c
Boma <bomaepele@yahoo.com>
2022-09-13 05:44:56 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* print_alphabet - prints lowercase alphabet
+* Return: 0
+*/
+int main(void)
+{
+char alpha;
+alpha = 'a';
+while (alpha <= 'z')
+{
+_putchar(alpha);
+alpha++;
+}
+_putchar('\n');
+return (0);
+}
