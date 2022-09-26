accc4ab4e18d3ff91957295a7eb0b1d588488fcc
Boma <bomaepele@yahoo.com>
2022-09-26 04:56:25 -0700

    2-strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stddef.h>
 /**
 * __strchr - locates a character in a string
 * @s: string
@@ -8,17 +8,12 @@
 */
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
+	int i;
+
+	for (i = 0; s[i] != '\n'; i++)
 	{
-		if (*s == c)
-		{
-			return (s);
-		}
-		++s
+		if (s[i] == c)
+			return (&s[i]);
 	}
-	if (*s == c)
-	{
-		return (s);
-	}
-	return (0);
+	return (NULL);
 }
