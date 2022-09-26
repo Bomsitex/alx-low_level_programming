e110d3b1b505f47585212a7b80f061993a7a2d7f
Boma <bomaepele@yahoo.com>
2022-09-26 08:41:21 -0700

    2-strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,13 +8,13 @@
 */
 char *_strchr(char *s, char c)
 {
-	int i = 0;
-
-	while (s[i] != '\0' && s[i] != c) /* find match */
-		i++;
-
-	if (s[i] == c) /* if match, assign to address */
-		return (&s[i]);
-	else
-		return (NULL);
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	return (0);
 }
