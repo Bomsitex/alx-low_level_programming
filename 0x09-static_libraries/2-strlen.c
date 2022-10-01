490ece1b1364a3eaffb6ff57ccac524c6ff11190
Boma <bomaepele@yahoo.com>
2022-09-30 20:13:38 -0700

    libmy

--- 0x09-static_libraries/2-strlen.c
+++ 0x09-static_libraries/2-strlen.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- * _strlen - Returns the length of a string.
- * @str: string.
- *
- * Return: Length.
- */
-size_t _strlen(const char *str)
+  * _strlen - Returns the length of a string
+  * @s: Input character
+  * Return: zero (Nothing)
+  */
+int _strlen(char *s)
 {
-	size_t len = 0;
+	int count = 0;
 
-	while (*str++)
-		len++;
-
-	return (len);
+	while (*(s + count) != '\0')
+	{
+		count++;
+	}
+	return (count);
 }
