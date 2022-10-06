3c7c1dbfaff18820851bd4d4e4e58358cb4c32b7
Boma <bomaepele@yahoo.com>
2022-10-06 08:00:05 -0700

    0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,18 +1,16 @@
-#include "main.h"
 #include <stdlib.h>
 
 /**
- * malloc_checked - This provides a safe way of mallocing
- * @b: This is the number of bytes to be malloced
- *
- * Return: Void Pointer
+ * *malloc_checked - allocates memory using malloc and exit if failed
+ * @b: int
+ * Return: pointer to the array initialized or NULL
  */
+
 void *malloc_checked(unsigned int b)
 {
-	void *ptr;
+int *m = malloc(b);
+if (m == 0)
+	exit(98);
 
-	ptr = malloc(b);
-	if (ptr == NULL)
-		exit(98);
-	return (ptr);
+return (m);
 }
