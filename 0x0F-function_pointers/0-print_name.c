15987f428948549fbe9feb16fae44246686afa68
Boma <bomaepele@yahoo.com>
2022-10-12 15:32:31 -0700

    main.h

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,13 +4,12 @@
 /**
  * print_name - Prints a name
  * @name: This is the name to be printed
- * @f: This is a pointer to a func
- *
- * Return: Null Void
+ * @f: This is a pointer to a func that print name
  */
 void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
 		return;
+
 	f(name);
 }
