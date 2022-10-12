fe7dc43a236998b7fdc8ab13db7e979133a40a0e
Boma <bomaepele@yahoo.com>
2022-10-12 13:27:24 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -5,12 +5,11 @@
  * print_name - Prints a name
  * @name: This is the name to be printed
  * @f: This is a pointer to a func
- *
- * Return: Null Void
+ * Return: Void
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL || f == NULL)
-		return;
-	f(name);
+if (name == NULL || f == NULL)
+return;
+f(name);
 }
