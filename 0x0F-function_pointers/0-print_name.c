edc837dc20af65bf4fe9de776d0dcc7affe3b014
Boma <bomaepele@yahoo.com>
2022-10-12 17:10:09 -0700

    update

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,7 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "function_pointers.h"
-#include "main.c"
 
 /**
  * print_name - print the name
@@ -11,6 +10,6 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (name != NULL && f != NUL)
+	if (name != NULL || f != NUL)
 		f(name);
 }
